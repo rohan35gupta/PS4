7b18930ee53bce20798c522f64081a79fde19a9d
Rohan Gupta <rohangupta@wustl.edu>
2018-02-22 01:44:45 -0600

    Getting started

--- problemset4.R
+++ problemset4.R
@@ -2,11 +2,14 @@
 #February 22, 2018
 #Pol Sci 4626 Problem Set 4
 
-myFunction<-function(doorthing, doorthing2, x){
-  doorthing1<-doorthing2<-sample(1:3, 1)
-  if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE }
-  x
+#Getting started
+myFunction<-function(doorChoice, carLocation){ #myFunction takes door choice & car location
+  if(doorChoice==carLocation){
+    x<-TRUE
+  }
+  else{
+    x<-FALSE
+  }
+  return(x) #Returns TRUE if equal & FALSE if not
 }
 myFunction(sample(1:3, 1), sample(1:3, 1))
-# Should return a TRUE if these samples are equal and
-# a false if they are not
