6ce52614b867df0483095f07d19d9025f2835112
Rohan Gupta <rohangupta@wustl.edu>
2018-02-22 01:58:23 -0600

    Moving on

--- problemset4.R
+++ problemset4.R
@@ -13,3 +13,19 @@ myFunction<-function(doorChoice, carLocation){ #myFunction takes door choice & c
   return(x) #Returns TRUE if equal & FALSE if not
 }
 myFunction(sample(1:3, 1), sample(1:3, 1))
+
+#1.
+setClass(Class="door", #Defined new S4 class door
+         representation = representation(
+           chosenDoor = "numeric", #chosenDoor integer (1, 2 or 3)
+           carDoor = "numeric", #carDoor integer (1, 2 or 3)
+           switch = "logical", #switch boolean indicating whether player adopted strategy of staying with first choice (FALSE) or switching to remaining door (TRUE)
+           winner = "logical" #winner boolean indicating whether or not final chosen door same as car door
+         ),
+         prototype = prototype(
+           chosenDoor = c(),
+           carDoor = c(),
+           switch = c(),
+           winner = c()
+         )
+         )
